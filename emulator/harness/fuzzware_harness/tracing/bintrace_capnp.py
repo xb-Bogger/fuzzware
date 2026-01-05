a4cb11cbfb85c3bb50579bc5f2d65ab4ed399616
xb-Bogger <bogger@foxmail.com>
2026-01-05 20:21:57 +0800

    fix bintrace_capnp.py

--- emulator/harness/fuzzware_harness/tracing/bintrace_capnp.py
+++ emulator/harness/fuzzware_harness/tracing/bintrace_capnp.py
@@ -10,11 +10,7 @@ import capnp as _capnp
 this_dir = _os.path.dirname(__file__)
 
 # modify your path!
-<<<<<<< HEAD
-bintrace_capnp = _capnp.load('/home/user/Desktop/web/backend/fuzzers/fuzzware/emulator/harness/fuzzware_harness/tracing/bintrace.capnp', imports=[this_dir])
-=======
 bintrace_capnp = _capnp.load('/home/xubo/fuzzers/fuzzware/emulator/harness/fuzzware_harness/tracing/bintrace.capnp', imports=[this_dir])
->>>>>>> 68332d5 (fix genconfig)
 
 TraceEvent = bintrace_capnp.TraceEvent
 Crash = bintrace_capnp.Crash
